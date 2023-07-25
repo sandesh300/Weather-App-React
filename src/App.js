19a1c13711b0d84fa3ade9303a8150724a10d787
sandesh <bhujbalsandesh52@gmail.com>
2023-07-25 13:40:19 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HomeComponents from './Components/Home';
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+  
+    <HomeComponents />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Home.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import WeekInfoCardComponents from './WeekInfoCard';
+import ChooseStateComponents from './ChooseState';
+import HumidityComponents from './Humidity';
+import LeftComponents from './Left';
+const HomeComponents = ()=>{   
+    return (
+        <>
+            <div className='homeWrap'>
+                <div className='weatherSection'>
+                    <LeftComponents />
+                    <div className='rightSide'>
+                        <ChooseStateComponents />
+                        <WeekInfoCardComponents />
+                        <HumidityComponents />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default HomeComponents;
+
